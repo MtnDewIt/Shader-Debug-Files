a43140feaebcbce6ee5dc2829e971ececc0bcaaf
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2024-05-26 20:10:59 +1000

    Add Chud Unknown Pixel Shader
    Matches the code for the chud_meter_gradient shader very closely, therefore it is being used as a base

--- ShaderGenerator/halo_online_shaders/chud_unknown.hlsl
+++ ShaderGenerator/halo_online_shaders/chud_unknown.hlsl
@@ -18,7 +18,7 @@
 //@generate chud_simple
 
 // ==== SHADER DOCUMENTATION
-// shader: chud_simple
+// shader: chud_unknown
 // 
 // ---- COLOR OUTPUTS
 // color output A= solid color
@@ -52,11 +52,110 @@ chud_output default_vs(vertex_type IN)
     return OUT;
 }
 
-// pixel fragment entry points
-
-// TODO: Create custom pixel shader code (Shares some similarities with chud_meter)
-// Temporary pixel shader function
-float4 default_ps(chud_output IN) : SV_Target
+float4 build_subpixel_result(float2 texcoord)
 {
-    return float4(1,2,3,4);	
+	float4 bitmap_result= sample2D(basemap_sampler, texcoord);
+#ifndef pc
+	asm{
+	tfetch2D bitmap_result, texcoord, basemap_sampler, MinFilter=linear, MagFilter=linear
+	};
+#else
+	bitmap_result= sample2D(basemap_sampler, texcoord);
+#endif
+	//float this_meter_value= 255.0*bitmap_result.b;
+	float this_meter_value= chud_scalar_output_EF.x*texcoord;
+	float edge_meter_value= chud_scalar_output_ABCD.x;
+	float gradient_width= chud_scalar_output_ABCD.y;
+	float edge_to_end= chud_scalar_output_EF.x - edge_meter_value; 
+	float gradient_min= edge_meter_value-gradient_width;
+	float gradient_max= edge_meter_value;
+	float blendout_gradient_width= gradient_width*0.25;
+	
+	if (edge_to_end<blendout_gradient_width)
+	{
+		gradient_min+= blendout_gradient_width-edge_to_end;
+		gradient_max+= blendout_gradient_width-edge_to_end;
+	}
+	
+	float flash_amount= 0;
+	
+	if (this_meter_value>gradient_min)
+	{
+		if (this_meter_value<=gradient_max)
+		{
+			flash_amount= (this_meter_value - gradient_min)/(gradient_max-gradient_min);
+		}
+		else
+		{
+			flash_amount= 1.0;
+		}		
+	}
+	
+	flash_amount= pow(flash_amount, 4.0);
+	
+	float4 result= chud_color_output_D;
+	result.a= bitmap_result.a*chud_scalar_output_ABCD.w;
+	
+	if (this_meter_value<=edge_meter_value)
+	{
+		float4 base_color= chud_color_output_A * (1.0f - bitmap_result.g) + chud_color_output_B*bitmap_result.g;
+	
+		result= base_color + chud_color_output_C*(flash_amount);
+		result.a= bitmap_result.a*(1.0 - flash_amount) + (bitmap_result.a*chud_scalar_output_ABCD.z)*flash_amount;
+	}
+	
+	//result.a*=chud_scalar_output_EF.w;
+		
+	return result;
+}
+
+float2 build_subsample_texcoord(float2 texcoord, float4 gradients, float dh, float dv)
+{
+	float2 result= texcoord;
+	result+= gradients.xz*dh;
+	result+= gradients.yw*dv;
+	
+	return result;
+}
+
+// pixel fragment entry points
+accum_pixel default_ps(chud_output IN) : SV_Target
+{
+#ifndef pc
+	//float subsample_scale= 1.0/9.0;
+	//float4 result= subsample_scale*build_subpixel_result(IN.Texcoord);
+
+	float4 gradients;
+	float2 texcoord= IN.Texcoord;
+	
+	asm {
+		getGradients gradients, texcoord, basemap_sampler 
+	};
+	
+	//gradients*=2.0;
+	
+	//result+= subsample_scale*build_subpixel_result(build_subsample_texcoord(texcoord, gradients, 1.0/3.0, -1.0/3.0));
+	//result+= subsample_scale*build_subpixel_result(build_subsample_texcoord(texcoord, gradients, 1.0/3.0, 0.0));
+	//result+= subsample_scale*build_subpixel_result(build_subsample_texcoord(texcoord, gradients, 1.0/3.0, 1.0/3.0));
+	//result+= subsample_scale*build_subpixel_result(build_subsample_texcoord(texcoord, gradients, -1.0/3.0, -1.0/3.0));
+	//result+= subsample_scale*build_subpixel_result(build_subsample_texcoord(texcoord, gradients, -1.0/3.0, 0.0));
+	//result+= subsample_scale*build_subpixel_result(build_subsample_texcoord(texcoord, gradients, -1.0/3.0, 1.0/3.0));
+	//result+= subsample_scale*build_subpixel_result(build_subsample_texcoord(texcoord, gradients, 0.0, -1.0/3.0));
+	//result+= subsample_scale*build_subpixel_result(build_subsample_texcoord(texcoord, gradients, 0.0, 1.0/3.0));
+	
+	float4 result= 0.0;
+	result+= build_subpixel_result(build_subsample_texcoord(texcoord, gradients, -2.0/9.0,  2.0/9.0));
+	result+= build_subpixel_result(build_subsample_texcoord(texcoord, gradients, -2.0/9.0, -2.0/9.0));
+	result+= build_subpixel_result(build_subsample_texcoord(texcoord, gradients,  2.0/9.0, -2.0/9.0));
+	result+= build_subpixel_result(build_subsample_texcoord(texcoord, gradients,  2.0/9.0,  2.0/9.0));
+	result /= 4.0;
+	result.a*=chud_scalar_output_EF.w;
+
+
+#else // pc
+	float4 result= build_subpixel_result(IN.Texcoord);
+	result.a*=chud_scalar_output_EF.w;
+#endif // pc
+
+	return chud_compute_result_pixel(result);
 }
