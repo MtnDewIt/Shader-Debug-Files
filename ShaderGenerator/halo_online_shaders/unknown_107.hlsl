699a294b98bbf32da180e6229a8d4b5f4262176f
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2024-06-17 11:00:55 +1000

    Update Unknown Shader

--- ShaderGenerator/halo_online_shaders/unknown_107.hlsl
+++ ShaderGenerator/halo_online_shaders/unknown_107.hlsl
@@ -22,27 +22,15 @@ screen_output default_vs(screen_output IN)
 
 accum_pixel default_ps(vertex_type IN) : SV_Target
 {
-    accum_pixel OUT;
+    float4 color_sample = sample2Dlod(color_sampler, IN.texcoord, 0);
+    float4 gbuf_sample = sample2Dlod(gbuf_sampler, IN.texcoord, 0);
 
-    float4 color_sample = sample2Dlod(color_sampler, float4(IN.texcoord, 0, 0));
-    float4 gbuf_sample = sample2Dlod(gbuf_sampler, float4(IN.texcoord, 0, 0));
+    color_sample.rgb *= color_sample.a;
+    color_sample.rgb *= gbuf_sample.rgb;
 
-    float3 color_rgb = color_sample.xyz * color_sample.w;
-    float3 combined_rgb = gbuf_sample.xyz * color_rgb;
-
-    float3 max_rgb = max(combined_rgb, float3(0, 0, 0));
-
-    float reciprocal_exposure = 1.0 / g_exposure.y;
-
-	OUT.color.xyz = max_rgb;
-    OUT.color.w = color_sample.w * g_exposure.w;
-
-    OUT.dark_color.xyz = max_rgb * reciprocal_exposure;
-	OUT.dark_color.w = color_sample.w * g_exposure.z;
-
+    return convert_to_render_target(color_sample, true, false
     #ifdef SSR_ENABLE
-	OUT.ssr_color = 0.0f;
+    , 0
     #endif
-
-    return OUT;
+    );
 }
