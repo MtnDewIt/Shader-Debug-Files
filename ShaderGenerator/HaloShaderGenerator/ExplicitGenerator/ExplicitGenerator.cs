710e5442422957b8674d53a3c70f3a1b212f1e47
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2024-08-16 13:58:37 +1000

    Add SSR Flag to Explicit Shaders
    Its easier to define the macro in the shader source itself, rather than the compiler

--- ShaderGenerator/HaloShaderGenerator/ExplicitGenerator/ExplicitGenerator.cs
+++ ShaderGenerator/HaloShaderGenerator/ExplicitGenerator/ExplicitGenerator.cs
@@ -98,19 +98,6 @@ namespace HaloShaderGenerator
             macros.Add(ShaderGeneratorBase.CreateMacro("disable_register_reorder", "1"));
             macros.Add(ShaderGeneratorBase.CreateMacro("EXPLICIT_COMPILER", "1"));
 
-            // While this technically goes unused in most cases, having SSR_ENABLE defined generates 1:1 shaders
-            switch (entryPoint)
-            {
-                case ShaderStage.Default:
-                case ShaderStage.Shadow_Apply:
-                case ShaderStage.Lightmap_Debug_Mode:
-                    macros.Add(ShaderGeneratorBase.CreateMacro("SSR_ENABLE", "1"));
-                    break;
-                default:
-                    macros.Add(ShaderGeneratorBase.CreateMacro("SSR_ENABLE", "0"));
-                    break;
-            }
-
             if (applyFixes)
                 macros.Add(ShaderGeneratorBase.CreateMacro("APPLY_FIXES", "1"));
         }
